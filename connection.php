3ecc01b9d872209f7e4fea93a9b1f5b962b93fe2
SilasVM <victorsmorgan@gmail.com>
2025-03-20 05:32:25 -0400

    Updated connection php file

--- connection.php
+++ connection.php
@@ -1,13 +1,16 @@
 <?php
 $host = "attendancedb.c9k8c68iecq2.us-east-2.rds.amazonaws.com";
-    $username = "admin";
-    $password = "Spartan$2025!";
-    $dbname = "attendancedb";
+$username = "admin";
+$password = "Spartan$2025!";
+$dbname = "attendancedb";
 
-    $con = mysqli_connect($host, $username, $password, $dbname);
+// Attempt to connect to the database
+$con = mysqli_connect($host, $username, $password, $dbname);
 
-    if (!$con = mysqli_connect($host, $username, $password, $dbname))
-    {
-        die("Connection failed!" . mysqli_connect_error());
-    }
+// Check the connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully!";
+}
 ?>
