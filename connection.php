a0680b38932aac17b9f54edac124f7302437f2ad
SilasVM <victorsmorgan@gmail.com>
2025-03-21 06:23:00 -0400

    Testing switching name of database from attendancedb to UserData

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $host = "admin@attendancedb.c9k8c68iecq2.us-east-2.rds.amazonaws.com";
 $username = "admin";
 $password = "Spartan$2025!";
-$dbname = "attendancedb";
+$dbname = "UserData";
 
 // Attempt to connect to the database
 $con = mysqli_connect($host, $username, $password, $dbname);
